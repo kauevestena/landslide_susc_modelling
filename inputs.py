9817077b848dba9199a1c697e7948e0239fee770
kalimap <kalimapitombeira@hotmail.com>
2025-09-27 20:07:44 -0300

    input paths were declared

---
+++ inputs.py
@@ -0,0 +1,9 @@
+#This is meant to hold the paths for input files
+# Training data
+ortophoto_train = r"C:\Users\Kalim\OneDrive\UFPR\Doutorado\Tese\Processamentos\IA\Training\Ortomosaico_train.tif"
+DTM_train = r"C:\Users\Kalim\OneDrive\UFPR\Doutorado\Tese\Processamentos\IA\Training\DTM_train.tif"
+ground_truth_train = r"C:\Users\Kalim\OneDrive\UFPR\Doutorado\Tese\Processamentos\IA\Training\Ground_truth_train.tif"
+
+# Test data
+ortophoto_test = r"C:\Users\Kalim\OneDrive\UFPR\Doutorado\Tese\Processamentos\IA\Test\Ortho_test.tif"
+DTM_test = r"C:\Users\Kalim\OneDrive\UFPR\Doutorado\Tese\Processamentos\IA\Test\DTM_4_GNSS-AAT.tif"
